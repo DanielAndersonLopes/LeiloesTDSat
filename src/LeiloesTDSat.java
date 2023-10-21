1fb629ea576bb6058eda8c298f7e8ba70c16d7a7
“Daniel <danielanderson.lopes0@gmail.com>
2023-10-21 14:53:37 -0300

    definindo a tela principal

---
+++ src/LeiloesTDSat.java
@@ -0,0 +1,9 @@
+public class LeiloesTDSat {
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+            new cadastroVIEW().setVisible(true);
+        });
+    }
+    
+}
