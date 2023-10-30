b75326c924215b72ba58deb6703ea70d223b4c93
“Daniel <danielanderson.lopes0@gmail.com>
2023-10-30 20:08:13 -0300

    Atualizar status

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -7,7 +7,6 @@
  *
  * @author Adm
  */
-
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
@@ -15,19 +14,18 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-
 public class ProdutosDAO {
-    
+
     Connection conn;
     PreparedStatement ps;
     ResultSet rs;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
+
     public ProdutosDAO() {
         this.conn = new conectaDAO().connectDB();
     }
-    
-    public void cadastrarProduto (ProdutosDTO produto){
+
+    public void cadastrarProduto(ProdutosDTO produto) {
         String sql = "insert into Produtos(nome, valor, status)" + "values(?,?,?)";
         try {
             ps = conn.prepareStatement(sql);
@@ -42,10 +40,10 @@ public class ProdutosDAO {
             }
         } catch (SQLException e) {
             JOptionPane.showMessageDialog(null, "Erro ao cadastrar.");
-        }        
+        }
     }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
+
+    public ArrayList<ProdutosDTO> listarProdutos() {
         try {
             String sql = "select * from Produtos";
             ps = conn.prepareStatement(sql);
@@ -67,9 +65,23 @@ public class ProdutosDAO {
             return null;
         }
     }
-    
-    
-    
-        
-}
 
+    public void venderProduto(int numeroId) {
+        try {
+            String sql = "UPDATE Produtos SET status = 'Vendido' WHERE id = ?";
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, numeroId);
+
+            int rowsUpdated = ps.executeUpdate();
+
+            if (rowsUpdated > 0) {
+                System.out.println("Produto vendido!");
+            } else {
+                System.out.println("Produto não encontrado.");
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro ao acessar os dados.");
+        }
+    }
+
+}
